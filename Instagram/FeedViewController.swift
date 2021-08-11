074c3a2fb0b1789e2b43f2565e7e0c5e5ca3972f
RebeccaVelez <contact@rebeccavelez.dev>
2021-08-11 15:24:46 -0400

    Update

--- Instagram/FeedViewController.swift
+++ Instagram/FeedViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class FeedViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    func onRefresh() {
+       
     }
     
 
